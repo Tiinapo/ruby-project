b6b6a0a3e0ea63b55375d7570a5a4a53e2e83473
Tiina Pohjolainen <tiina.pohjolainen@mail.com>
2018-04-08 22:08:44 +0300

    Tagging feature completed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+	protect_from_forgery with: :exception
 
   def show
   	@article = Article.find(params[:id])
